632e9569a28232d0f9fd3777ff43a535c36e6e0c
Josue Giovanni Valdivia Urrutia <jvaldiviau@fi.uba.ar>
2026-05-12 15:03:40 -0300

    fix: update import path for send_email in users.py

--- src/backend/app/users/routes/users.py
+++ src/backend/app/users/routes/users.py
@@ -1,5 +1,5 @@
 # from backend.app.services.mail_service import send_email
-from app.services.mail_service import send_email
+from  app.core.mail_service import send_email
 from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException, status, Body
 from sqlalchemy.orm import Session
 
